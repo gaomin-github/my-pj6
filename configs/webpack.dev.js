72f20c483a97eedfd84de8c908c495b4c74a6670
GMfight <374204501@qq.com>
2019-04-02 22:13:29 +0800

    img source handle
    图片资源引入配置

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -7,6 +7,8 @@ const OptimizeCSSAssetsPlugin=require('optimize-css-assets-webpack-plugin');
 const MinCssExtractPlugin=require('mini-css-extract-plugin');
 const UglifyJsPlugin=require('uglifyjs-webpack-plugin')
 module.exports={
+    mode:'development',
+    // mode:'production',
     entry:{
         main:["./src/main.js"],
         main1:["./src/main1.js"]
@@ -16,27 +18,22 @@ module.exports={
         path:path.resolve(__dirname,'../built'),
     },
     // 扩容
-    // performance:{
-    //   hints:'warning',
-    //   maxEntrypointSize:5000000,
-    //   maxAssetSize:3000000
-    // },
+    performance:{
+      hints:'warning',
+      maxEntrypointSize:5000000,
+      maxAssetSize:3000000
+    },
     devServer:{
-        // historyApiFallback: {
-        //     rewrites: [
-        //         { from: /.*/, to: '/built/index.html' },
-        //     ],
-        // },
         historyApiFallback:true,
         clientLogLevel:'info',
         host:'localhost',
         port:8086,
-        open:true,
-        contentBase:'./built',
+        open:false,
+        contentBase:path.resolve(__dirname,'../'),
         publicPath:'/',
     },
     resolve:{
-      extensions:['.js','.json','.tsx','.ts','.vue'],
+      extensions:['.js','.json','.tsx','.ts','.vue','.jpg'],
       alias:{
           'vue$':'vue/dist/vue.esm.js'
       }
@@ -59,45 +56,59 @@ module.exports={
           {
               test:/\.(vue)?$/,
               loader:'vue-loader',
-              exclude:/node_modules/
+              exclude:/node_modules/,
+              include:[
+                  path.resolve(__dirname,'../src')
+              ]
           },
           {
               test:/\.(css|scss)$/,
               use:[
-                  MinCssExtractPlugin.loader,
+                  'style-loader',
                   'css-loader',
                   'sass-loader'
               ],
-              exclude:/node_modules/
+              exclude:/node_modules/,
+              include:[
+                  path.resolve(__dirname,'../src')
+              ]
+          },
+          {
+              test:/\.(jpg|png)$/,
+              use:['url-loader'],
+              include:[
+                  path.resolve(__dirname,'../src'),
+                  path.resolve(__dirname,'../static')
+              ]
           }
       ]
     },
     devtool:'inline-source-map',
 
-    optimization:{
-        // 抽取共用代码
-      splitChunks:{
-          cacheGroups:{
-              commons:{
-                  chunks:'all',
-                  name:'vendor',
-                  test:/[\\/]node_modules[\\/]/
-              }
-          }
-      },
-        // 默认开启压缩代码
-      minimizer:[
-          // 优化合并压缩css
-          new OptimizeCSSAssetsPlugin({}),
-          // production模式下，webpack4会自动压缩js文件
-          // js文件压缩插件
-          new UglifyJsPlugin({
-              cache:true,
-              parallel:true,
-              sourceMap:true
-          })
-      ]
-    },
+    // optimization:{
+    //     // 抽取共用代码
+    //   splitChunks:{
+    //       cacheGroups:{
+    //           commons:{
+    //               chunks:'all',
+    //               name:'vendor',
+    //               test:/[\\/]node_modules[\\/]/
+    //           }
+    //       }
+    //   },
+    //     // 默认开启压缩代码
+    //   minimizer:[
+    //       // 优化合并压缩css
+    //       new OptimizeCSSAssetsPlugin({}),
+    //       // production模式下，webpack4会自动压缩js文件
+    //       // js文件压缩插件
+    //       new UglifyJsPlugin({
+    //           cache:true,
+    //           parallel:true,
+    //           sourceMap:true
+    //       })
+    //   ]
+    // },
     plugins:[
         new CleanWebpackPlugin(['built'],{
             root:path.resolve(__dirname,'..'),
@@ -109,8 +120,9 @@ module.exports={
         }),
         new webpack.HotModuleReplacementPlugin(),
         new VueLoaderPlugin(),
-        new MinCssExtractPlugin({
-            filename:'[name].[hash].css'
-        })
-    ]
+        // new MinCssExtractPlugin({
+        //     filename:'[name].[hash].css'
+        // })
+    ],
+    stats:'none'
 }

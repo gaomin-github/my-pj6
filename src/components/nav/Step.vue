0081419fcf8fb4f7f4731b3600488faed0b2e9c4
GMfight <374204501@qq.com>
2019-04-09 22:38:10 +0800

    step component
    step component

--- src/components/nav/Step.vue
+++ src/components/nav/Step.vue
@@ -1,8 +1,12 @@
 <template>
     <section class="container">
-        <section v-for="(item,key) in dataSource" :key="key">
-            {{item.label}}
-        </section>
+        <!--<section class="step_item" v-for="(item,key) in dataSource" :key="key">-->
+           <!--<section class="step_key">{{key}}</section>-->
+            <!--<section class="step_label">-->
+                <!--{{item.label}}-->
+            <!--</section>-->
+        <!--</section>-->
+        <section class="step_key"><span>1</span>1</section>
     </section>
 </template>
 <script lang="ts">
@@ -28,7 +32,19 @@
 </script>
 <style lang="scss" scoped>
     .container{
-        width:60px;
+        /*width:60px;*/
+        /*height:30px;*/
+        margin-left: 20px;
+    }
+    .step_key{
+        width:30px;
         height:30px;
+        float:left;
+        /*border-radius: 15px;*/
+        border:1px rgb(50,50,50) solid;
+        text-align: center;
+        vertical-align: bottom;
+        /*line-height: 30px;*/
+        /*position: absolute;*/
     }
 </style>
